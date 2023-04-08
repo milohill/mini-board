4dfe85290e65d7e0c8b4a202ed8a77d99ca5ca66
milohill <116620023+grimhilt7@users.noreply.github.com>
2023-04-08 11:46:50 +0900

    Fix a typo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+const mongoose = require('mongoose');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
