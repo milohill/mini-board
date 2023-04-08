9982fa832d5611f27dead28e56ee879242e10e25
milohill <116620023+grimhilt7@users.noreply.github.com>
2023-04-08 11:59:58 +0900

    Configure mongoose

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+require('dotenv').config();
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URL);
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
